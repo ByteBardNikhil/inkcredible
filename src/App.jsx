159f69b6903113fa5d4c69c5cba03a379e4f4d15
Nikhil-197 <mahajannikhil177@gmail.com>
2025-09-04 16:49:30 +0530

    Added HashRouter

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { HashRouter as Router, Routes, Route } from "react-router-dom";
 import Navbar from "./layouts/Navbar";
 import Quotation from "./Modules/Quotation";
 import Orders from "./Modules/Order";
