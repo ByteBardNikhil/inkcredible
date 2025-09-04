9f98751d07a869c7468c288a446f8ecbdc7c2898
Nikhil-197 <mahajannikhil177@gmail.com>
2025-09-04 09:53:32 +0530

    fix: set base for GitHub Pages

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   plugins: [react()],
-  base: "/InkCredible/", // 👈 GitHub repo name
+  base: "/inkcredible/", // 👈 GitHub repo name
   build: {
     outDir: "dist",
     emptyOutDir: true,
